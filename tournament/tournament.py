3daa806ba033800d09aa26bbb5258317f06bad84
Kalyn Warnock <hackzor12@users.noreply.github.com>
2017-03-21 17:52:15 -0700

    Add files via upload

---
+++ tournament/tournament.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# 
+# tournament.py -- implementation of a Swiss-system tournament
+#
+
+import psycopg2
+import bleach
+
+
+def connect():
+    """connect to the PostgreSQL database.  Returns a database connection."""
+    return psycopg2.connect("dbname=tournament")
+
+
+def deleteMatches():
+    db = connect()
+    cursor= db.cursor()
+    cursor.execute("DELETE FROM Matches;")
+    db.commit()
+    """Remove all the match records from the database."""
+
+
+def deletePlayers():
+    db = connect()
+    cursor= db.cursor()
+    cursor.execute("DELETE FROM Players;")
+    db.commit()
+    """Remove all the player records from the database."""
+
+
+def countPlayers():
+    db = connect()
+    cursor= db.cursor()
+    cursor.execute("SELECT count(id) FROM Players;")
+    return cursor.fetchall()[0][0]
+    """Returns the number of players currently registered."""
+
+
+def registerPlayer(name):
+    db = connect()
+    cursor= db.cursor()
+    name = bleach.clean(name, strip =True)
+    cursor.execute("INSERT INTO Players (name) VALUES (%s)" ,
+        (name,))
+    db.commit()
+    """Adds a player to the tournament database.
+  
+    The database assigns a unique serial id number for the player.  (This
+    should be handled by your SQL database schema, not in your Python code.)
+  
+    Args:
+      name: the player's full name (need not be unique).
+    """
+
+
+def playerStandings():
+    db = connect()
+    cursor= db.cursor()
+    cursor.execute("SELECT * FROM Standings;")
+    return cursor.fetchall()
+    """Returns a list of the players and their win records, sorted by wins.
+
+    The first entry in the list should be the player in first place, or a player
+    tied for first place if there is currently a tie.
+
+    Returns:
+      A list of tuples, each of which contains (id, name, wins, matches):
+        id: the player's unique id (assigned by the database)
+        name: the player's full name (as registered)
+        wins: the number of matches the player has won
+        matches: the number of matches the player has played
+    """
+
+
+def reportMatch(winner, loser):
+    db = connect()
+    cursor= db.cursor()
+    cursor.execute("INSERT INTO Matches (winner,loser) VALUES(%s,%s)" ,
+        (winner,loser))
+    db.commit()
+    """Records the outcome of a single match between two players.
+
+    Args:
+      winner:  the id number of the player who won
+      loser:  the id number of the player who lost
+    """
+ 
+ 
+def swissPairings():
+    standings = playerStandings()
+    swiss_pairs = []
+    for i in range(0,len(standings),2):
+        swiss_pairs.append((standings[i][0],standings[i][1],standings[i+1][0],standings[i+1][1]))
+    return swiss_pairs
+
+    """Returns a list of pairs of players for the next round of a match.
+  
+    Assuming that there are an even number of players registered, each player
+    appears exactly once in the pairings.  Each player is paired with another
+    player with an equal or nearly-equal win record, that is, a player adjacent
+    to him or her in the standings.
+  
+    Returns:
+      A list of tuples, each of which contains (id1, name1, id2, name2)
+        id1: the first player's unique id
+        name1: the first player's name
+        id2: the second player's unique id
+        name2: the second player's name
+    """
+
+
---
+++ tournament/tournament.sql
@@ -0,0 +1,34 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+
+
+--Use psql tournament to connect and run these commands
+--Create a database called tournament
+CREATE DATABASE tournament;
+
+--Create a table called players with an auto incrementing id and a text field name
+CREATE TABLE Players(
+id serial PRIMARY KEY,
+name varchar(20)
+);
+
+--Create a table called Matches with an auto incrementing id, an int field winner, and and int field loser
+CREATE TABLE Matches(
+id serial PRIMARY KEY,
+winner int,
+loser int,
+);
+
+--Create a view called Standings so we can do operations on it later
+CREATE VIEW Standings AS
+SELECT p.id as id, p.name as name,
+(SELECT count(*) FROM Matches WHERE Matches.winner = p.id) as won,
+(SELECT count(*) FROM Matches WHERE p.id in (winner, loser)) as matches
+FROM Players p
+GROUP BY p.id
+ORDER BY won DESC;
